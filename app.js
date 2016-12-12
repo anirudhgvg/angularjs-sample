2088912a90febbb59ac3def867af1f2b9c8feaa2
anirudhgvg <anirudh.gudivada@gmail.com>
2016-12-12 00:45:16 -0800

    Bug Fix

--- app.js
+++ app.js
@@ -5,22 +5,25 @@ angular.module('LunchCheck', [])
 .controller('LunchCheckController', function ($scope) {
     $scope.dishes = ""; 
     $scope.message = ""; 
-    $scope.msgColor="green"
+    $scope.msgColor="black";
 
     $scope.check = function () {        
         var ndishes = $scope.dishes.trim();
-        if(ndishes.length < 1)
+        if(ndishes.length < 1) {
             $scope.message = "";
-        ndishes = ndishes.split(',');    
-        if(ndishes.length <= 3) {
-            $scope.message = "Enjoy!";
-            $scope.msgColor="green";
-        }            
-        else if(ndishes.length > 3) {
-            $scope.message = "Too much!";
-            $scope.msgColor = "red";
+            $scope.msgColor="black";
         }
-            
+        else {
+            ndishes = ndishes.split(',');    
+            if(ndishes.length <= 3) {
+                $scope.message = "Enjoy!";
+                $scope.msgColor="green";
+            }            
+            else if(ndishes.length > 3) {
+                $scope.message = "Too much!";
+                $scope.msgColor = "red";
+            }
+        }            
     };
 });
 
